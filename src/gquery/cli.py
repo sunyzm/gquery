ddd863d57be0661bb7b3878173601651adb9584e
sunyzm <mingzh26@gmail.com>
2023-10-08 12:20:52 -0700

    Implement CLI with the click package.

--- src/gquery/cli.py
+++ src/gquery/cli.py
@@ -1,12 +1,14 @@
 from gquery import AirportInfo, CityInfo, compute_coord_distance, LengthUnit, GQueryEngine
+import click
 import pyinputplus as pyip
-import sys
+
+
+_query_engine = GQueryEngine()
 
 
 def find_city(query_engine: GQueryEngine, city_name: str) -> CityInfo | None:
     matched_cities = query_engine.find_cities(city_name)
     if len(matched_cities) == 0:
-        print("No matched city is found.")
         return None
     elif len(matched_cities) == 1:
         return matched_cities[0]
@@ -32,50 +34,55 @@ def find_place(query_engine: GQueryEngine, name: str) -> AirportInfo | CityInfo 
     return find_city(query_engine, name)
 
 
-def main():
-    argv = sys.argv
-    query_engine = GQueryEngine()
+@click.group()
+def cli():
+    pass
 
-    match argv[1:]:
-        case ("info", *names):
-            for name in names:
-                if matched_place := find_place(query_engine, name):
-                    print(matched_place)
-        case ("distance", name1, name2, *extra_arg):
-            place1 = find_place(query_engine, name1)
-            if place1 is None:
-                print("Failed to find {name1}")
-                exit(1)
 
-            place2 = find_place(query_engine, name2)
-            if place2 is None:
-                print("Failed to find {name2}")
-                exit(1)
+@click.command()
+@click.argument("names", nargs=-1)
+def info(names):
+    for name in names:
+        if matched_place := find_place(_query_engine, name):
+            print(matched_place)    
 
-            unit = LengthUnit.KM
-            for arg in extra_arg:
-                if arg.startswith("--unit="):
-                    unit_str = arg.split(sep="=", maxsplit=1)[1].lower()
-                    match unit_str:
-                        case "mi" | "mile":
-                            unit = LengthUnit.MI
-                        case "km" | "kilometer":
-                            unit = LengthUnit.KM
-                        case _:
-                            print(f"Unrecognized unit {unit}")
-                            exit(1)
 
-            distance, unit_symbol = compute_coord_distance(
-                place1.coord, place2.coord, unit
-            )
-            print(
-                f"Distance between {place1.name} and "
-                f"{place2.name}: {distance:.1f} {unit_symbol}"
-            )
+@click.command()
+@click.argument("names", nargs=2)
+@click.option("--unit", default="km", help="Unit of distance (km or mi)")
+def distance(names, unit):
+    place1 = find_place(_query_engine, names[0])
+    if place1 is None:
+        print(f"Failed to find {names[0]}")
+        exit(1)
+
+    place2 = find_place(_query_engine, names[1])
+    if place2 is None:
+        print(f"Failed to find {names[1]}")
+        exit(1)
+
+    length_unit = LengthUnit.KM
+    match unit:
+        case "mi" | "mile":
+            length_unit = LengthUnit.MI
+        case "km" | "kilometer":
+            length_unit = LengthUnit.KM
         case _:
-            print("Unrecognized command")
+            print(f"Unrecognized unit {unit}")
             exit(1)
 
+    distance, unit_symbol = compute_coord_distance(
+                place1.coord, place2.coord, length_unit
+    )
+    print(
+        f"Distance between {place1.name} and "
+        f"{place2.name}: {distance:.1f} {unit_symbol}"
+    )
 
-if __name__ == "__main__":
-    main()
+
+cli.add_command(info)
+cli.add_command(distance)
+
+
+def main():
+    cli()
